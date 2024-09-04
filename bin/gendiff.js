ad337b52dbf879adfec928221279ac868f37a6ee
Andrey-Ryabchikov <a_ryabchikov@inbox.ru>
2024-09-04 23:26:28 +0300

    Updated json

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,1 +1,12 @@
-console.log('Hello');
+#!/usr/bin/env node
+
+import { program } from 'commander';
+const program = new Command();
+
+program
+    .description('Compares two configuration files and shows a difference.');
+    .version('1.0.0');
+
+program.helpOption('-h, --help', 'output usage information');
+
+program.parse();
