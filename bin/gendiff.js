1587ec16e5ca7d1e3bc24ba9d44e12fbead1c7cb
Andrey-Ryabchikov <a_ryabchikov@inbox.ru>
2024-09-04 23:49:27 +0300

    First step

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,10 +1,9 @@
 #!/usr/bin/env node
 
 import { program } from 'commander';
-const program = new Command();
 
 program
-    .description('Compares two configuration files and shows a difference.');
+    .description('Compares two configuration files and shows a difference.')
     .version('1.0.0');
 
 program.helpOption('-h, --help', 'output usage information');
