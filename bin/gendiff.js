62fab0e5e202c84471632512720439dfc931bdce
Andrey-Ryabchikov <a_ryabchikov@inbox.ru>
2024-09-05 00:12:29 +0300

    Second step

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,7 +4,9 @@ import { program } from 'commander';
 
 program
     .description('Compares two configuration files and shows a difference.')
-    .version('1.0.0');
+    .version('1.0.0')
+    .arguments('<filepath1> <filepath2>')
+    .option('-f, --format [type]', 'output format'); 
 
 program.helpOption('-h, --help', 'output usage information');
 
