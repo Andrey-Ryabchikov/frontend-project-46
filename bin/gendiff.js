65a282f48ede06706a7f153b27262284bc1bf2dd
Andrey-Ryabchikov <a_ryabchikov@inbox.ru>
2024-09-17 02:04:12 +0300

    return to second step

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -2,18 +2,10 @@
 
 import { program } from 'commander';
 
-import gendiff from '../src/Index.js';
-
-gendiff('__fixtures__/file1.json', '__fixtures__/file2.json');
-
 program
     .description('Compares two configuration files and shows a difference.')
     .version('1.0.0')
-    .arguments('<filepath1> <filepath2>')
-    .option('-f, --format [type]', 'output format')
-    .action((filepath1, filepath2) => {
-        console.log(gendiff(filepath1, filepath2)); 
-    });
+    .option('-f, --format [type]', 'output format');
 
 program.helpOption('-h, --help', 'output usage information');
 
--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-import gendiff from "./src/parser.js";
+import gendiff from "./src/index.js";
 
 export default gendiff;
