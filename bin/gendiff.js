307bd975928043dfbec15585a9fec1b0a82c0a69
Andrey-Ryabchikov <a_ryabchikov@inbox.ru>
2024-09-19 02:32:40 +0300

    Second step mod.2

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -2,13 +2,18 @@
 
 import { program } from 'commander';
 
+import gendiff from "../src/index.js";
+
+gendiff('__fixtures__/file1.json', '__fixtures__/file2.json');
 
 program
     .description('Compares two configuration files and shows a difference.')
     .version('1.0.0')
     .argument('<filepath1> <filepath2>')
-    .option('-f, --format [type]', 'output format'),
+    .option('-f, --format [type]', 'output format')
+
 
 program.helpOption('-h, --help', 'output usage information');
 
 program.parse(process.argv);
+
--- src/index.js
+++ src/index.js
@@ -1,5 +1,26 @@
+
+import fs from 'fs';
+import path from 'path';
+import parse from './parse.js';
+
+
+
+const getFullPath = (filepath) => path.resolve(process.cwd(), filepath);
+const extractFormat = (filepath) => path.extname(filepath).slice(1);
+const getData = (filepath) => parse(fs.readFileSync(filepath, ('utf-8')), extractFormat(filepath))
+
 const gendiff = (filepath1, filepath2) => {
-    console.log(filepath1, filepath2)
+ 
+  const fullFilePath1 = getFullPath(filepath1);
+  const fullFilePath2 = getFullPath(filepath2);
+
+  const data1 = getData(fullFilePath1);
+  const data2 = getData(fullFilePath2);
+
+  console.log(data1);
+  console.log(data2);
+  
 }
+ 
 
 export default gendiff;
