c797a7321918a4e428fe2d6b23a0708d794a1850
Andrey-Ryabchikov <a_ryabchikov@inbox.ru>
2025-01-10 01:12:49 +0300

    tests updated

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -10,21 +10,19 @@ const __dirname = dirname(__filename);
 const getFixturePath = (filename) => join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
 
-describe('Comparison checks', () => {
-  test.each([
-    ['file1.json', 'file2.json', 'expectedResultStylish.txt'],
-    ['file1.yaml', 'file2.yaml', 'expectedResultStylish.txt'],
-  ])('Comparison check for %s and %s with default format', (file1, file2, expectedResultFile) => {
-    const received = gendiff(getFixturePath(file1), getFixturePath(file2));
-    const expected = readFile(expectedResultFile);
-    expect(received).toEqual(expected);
-  });
+test.each([
+  ['file1.json', 'file2.json', 'expectedResultStylish.txt'],
+  ['file1.yaml', 'file2.yaml', 'expectedResultStylish.txt'],
+])('Comparison check for %s and %s with default format', (file1, file2, expectedResultFile) => {
+  const received = gendiff(getFixturePath(file1), getFixturePath(file2));
+  const expected = readFile(expectedResultFile);
+  expect(received).toEqual(expected);
+});
 
-  test.each([
-    ['file1.yaml', 'file2.yaml', 'expectedResultPlain.txt'],
-  ])('Gendiff plain Json for %s and %s', (file1, file2, expectedResultFile) => {
-    const received = gendiff(getFixturePath(file1), getFixturePath(file2), 'plain');
-    const expected = readFile(expectedResultFile);
-    expect(received).toEqual(expected);
-  });
+test.each([
+  ['file1.yaml', 'file2.yaml', 'expectedResultPlain.txt'],
+])('Gendiff plain Json for %s and %s', (file1, file2, expectedResultFile) => {
+  const received = gendiff(getFixturePath(file1), getFixturePath(file2), 'plain');
+  const expected = readFile(expectedResultFile);
+  expect(received).toEqual(expected);
 });
