5d9c3dab2e57ad7bf5fb7db24cad77eb190e96c0
Andrey-Ryabchikov <a_ryabchikov@inbox.ru>
2025-01-10 01:25:51 +0300

    parse updated

--- src/parse.js
+++ src/parse.js
@@ -1,13 +1,5 @@
 import yaml from 'js-yaml';
 
-// const parsers = {
-//   json: JSON.parse,
-//   yaml: yaml.load,
-//   yml: yaml.load,
-// };
-
-// export default (data, format) => parsers[format](data);
-
 const parse = (file, extension) => {
   switch (extension) {
     case 'json':
