c40b47728d2a34ce0bee4e30082374dc685c5d21
Andrey-Ryabchikov <a_ryabchikov@inbox.ru>
2025-01-10 01:32:35 +0300

    index updated

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -2,13 +2,15 @@ import plain from './plain.js';
 import stylish from './stylish.js';
 import jsonFormat from './jsonFormat.js';
 
-export default (diff, formatName) => {
+export default (diff, formatName = 'stylish') => {
   switch (formatName) {
     case 'plain':
       return plain(diff);
     case 'json':
       return jsonFormat(diff);
-    default:
+    case 'stylish':
       return stylish(diff);
+    default:
+      throw new Error(`Unknown format: '${formatName}'`);
   }
 };
